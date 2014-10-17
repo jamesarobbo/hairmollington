921c9833dbaffffe0cd242a9db794c085052b236
James Robinson <jamesarobinson6@gmail.com>
2014-10-17 23:34:19 +0100

    fix unclickable links for mobile

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 	$(".menu").on("click", function(){
+
+		$(".services-content").css("margin-top", "240px");
 		$("nav.mobile").toggle();
+
+		if ($("nav.mobile").is(":visible")) {
+			$(".services-content").css("margin-top", "260px")
+			$(".work-content").css("margin-top", "260px")
+		}
+		else {
+			$(".services-content").css("margin-top", "0px")
+			$(".work-content").css("margin-top", "180px")
+
+		}
+
 	});
 });
